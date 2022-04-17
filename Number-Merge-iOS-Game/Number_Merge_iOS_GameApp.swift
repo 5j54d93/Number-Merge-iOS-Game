5bd76aa5e74c918cde2abfb5ed6274cc062e8eef
Ricky Chuang <songsongyyohya@gmail.com>
2022-04-17 19:44:19 +0800

    Initial Commit

---
+++ Number-Merge-iOS-Game/Number_Merge_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_Merge_iOS_GameApp.swift
+//  Number-Merge-iOS-Game
+//
+//  Created by 莊智凱 on 2022/4/17.
+//
+
+import SwiftUI
+
+@main
+struct Number_Merge_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
