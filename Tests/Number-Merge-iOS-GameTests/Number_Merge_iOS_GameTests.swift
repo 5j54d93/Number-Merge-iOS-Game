7aa34835f2d228f40ee44e8e05000174db625442
Ricky Chuang <songsongyyohya@gmail.com>
2022-04-19 21:46:51 +0800

    Create Number_Merge_iOS_GameTests.swift

---
+++ Tests/Number-Merge-iOS-GameTests/Number_Merge_iOS_GameTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Number_Merge_iOS_Game
+
+final class Number_Merge_iOS_GameTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Number_Merge_iOS_Game().text, "Hello, World!")
+    }
+}
